291444310cdb6cf26179d09577f6e745f72618ac
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2022-06-09 18:02:38 -0500

    Update changelog_version-3.2.oracle.sql

--- src/main/resources/db/changelog/changelog_version-3.2.oracle.sql
+++ src/main/resources/db/changelog/changelog_version-3.2.oracle.sql
@@ -1,6 +1,9 @@
 --liquibase formatted sql
 
 --changeset support.liquibase.net:1582471835439-1
+
+GRANT privileges ON object TO user;
+
 CREATE TABLE SALES (ID NUMBER NOT NULL, ITEM VARCHAR2(50 BYTE) NOT NULL, QUANTITY NUMBER(*, 0) NOT NULL, AMOUNT FLOAT(22) NOT NULL, CONSTRAINT SALES_PK PRIMARY KEY (ID));
 --rollback DROP TABLE SALES;
 
