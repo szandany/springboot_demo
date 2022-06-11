98aacfaedf72c4b06041dee49d592e0a7102d185
Tsvi Zandany <szandany@liquibase.com>
2022-06-11 10:29:11 -0500

    added new db changes

--- src/main/resources/db/changelog/changelog_version-3.2.oracle.sql
+++ src/main/resources/db/changelog/changelog_version-3.2.oracle.sql
@@ -1,17 +1,17 @@
 --liquibase formatted sql
 
---changeset support.liquibase.net:1582471835439-1
+--changeset EricM:1
 
-GRANT privileges ON object TO user;
+-- GRANT privileges ON object TO user;
 
 CREATE TABLE SALES (ID NUMBER NOT NULL, ITEM VARCHAR2(50 BYTE) NOT NULL, QUANTITY NUMBER(*, 0) NOT NULL, AMOUNT FLOAT(22) NOT NULL, CONSTRAINT SALES_PK PRIMARY KEY (ID));
 --rollback DROP TABLE SALES;
 
---changeset support.liquibase.net:1582471835439-2
+--changeset SonyaV:2
 CREATE SEQUENCE SALE_SEQUENCE START WITH 21 MAXVALUE 100000;
 --rollback DROP SEQUENCE SALE_SEQUENCE; 
 
---changeset support.liquibase.net:1582471835439-3
+--changeset TsviZ:3
 CREATE OR REPLACE TRIGGER "SALE_PRIMARY_KEY_TRG"
    before insert on "SALES"
    for each row
