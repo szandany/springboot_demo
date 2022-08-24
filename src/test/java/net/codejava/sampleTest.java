b35367169a95072037ce3e902b6e6e1cddda38f5
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-08-24 16:58:08 -0500

    added new test job

--- src/test/java/net/codejava/sampleTest.java
+++ src/test/java/net/codejava/sampleTest.java
@@ -1,14 +1,14 @@
 package net.codejava;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest
+// @SpringBootTest
 class JUnit5ExampleTest {
 
     @Test
     void justAnExample() {
-      System.out.println("\n\nTest successful\n\n");
+      System.out.println("\n\nTest1 successful\n\n");
     }
 
     @Test
